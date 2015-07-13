a18853462c9aa4f03a7a6de6944ab96add262e39
Mário César Melon Bomfim <mariocesarbomfim@gmail.com>
2015-07-13 18:39:52 -0300

    Update Polinomios.c

--- Polinomios.c
+++ Polinomios.c
@@ -18,10 +18,10 @@ No* transformaString(char* str, No* lista)
             apagarParteString(str, 1);
         }
 
-        copiaParteNumerica(str, strCoef);                /*copia o coeficiente para a string strNumero*/
+        copiaParteNumerica(str, strCoef);                   /*copia o coeficiente para a string strNumero*/
 
-        if(strlen(strCoef) == 0) coef = 1;                /*verifica se possui coeficiente*/
-        else apagarParteString(str, strlen(strCoef));          /*apaga a string strNumero do começo de str*/
+        if(strlen(strCoef) == 0) coef = 1;                  /*verifica se possui coeficiente*/
+        else apagarParteString(str, strlen(strCoef));       /*apaga a string strNumero do começo de str*/
 
         if(str[0] == 'x')                                   /*verifica se o primeiro caractere de str é 'x'*/
         {
@@ -87,6 +87,35 @@ void copiaParteNumerica(char* str1, char* str2)
     str2[i] = '\0';
 }
 
+/* A função insere os dois polinômios a serem somados e
+depois simplifica-os já que a inserção é ordenada*/
+No *somaPolinomio(No *l1,No *l2)
+{
+
+    No *l3,*p1,*p2;
+
+    l3=criaLista();
+    p1=l1;
+    p2=l2;
+
+    while(p1!=NULL || p2!=NULL)
+    {
+        if(p1!=NULL)
+        {
+            l3=insereOrdenado(l3,p1->coef,p1->exp);
+            p1=p1->proximo;
+        }
+        if(p2!=NULL)
+        {
+            l3=insereOrdenado(l3,p2->coef,p2->exp);
+            p2=p2->proximo;
+        }
+    }
+    imprimeLista(l3);
+    l3=simplificaPolinomio(l3);
+    return l3;
+}
+
 No* subtrairPolinomios(No* polinomio1, No* polinomio2)
 {
     No *polinomioResultante, *p1, *p2;
@@ -96,58 +125,58 @@ No* subtrairPolinomios(No* polinomio1, No* polinomio2)
     p1 = polinomio1;
     p2 = polinomio2;
 
-    if(polinomio1 == NULL && polinomio2 == NULL) return NULL;                   /*testa se os polinomios nao sao nulos*/
+    if(polinomio1 == NULL && polinomio2 == NULL) return NULL;       /*testa se os polinomios nao sao nulos*/
 
-    if(polinomio1 != NULL && polinomio2 == NULL) return polinomio1;              /*se polinomio2 é nulo, retorna polinomio 1*/
+    if(polinomio1 != NULL && polinomio2 == NULL) return polinomio1; /*se polinomio2 é nulo, retorna polinomio 1*/
 
-    if(polinomio1 == NULL && polinomio2 != NULL) return polinomio2;              /*se polinomio1 é nulo, retorna polinomio 2*/
+    if(polinomio1 == NULL && polinomio2 != NULL) return polinomio2; /*se polinomio1 é nulo, retorna polinomio 2*/
 
-    while(p1 != NULL || p2 != NULL)                                              /*percorres as listas ate o fim das duas*/
+    while(p1 != NULL || p2 != NULL)                                 /*percorres as listas ate o fim das duas*/
     {
-        if(p1 != NULL && p2 != NULL)                                             /*Se p1 e p2 forem diferentes de nulo*/
+        if(p1 != NULL && p2 != NULL)                         /*Se p1 e p2 forem diferentes de nulo*/
         {
 
-            if(p1->exp == p2->exp)                                               /*Se possuirem expoentes iguais*/
+            if(p1->exp == p2->exp)                          /*Se possuirem expoentes iguais*/
             {
-                if((p1->coef - p2->coef) != 0)                                  /*se o coeficiente p1 menos o
-                                                                                coeficiente de p2 não for igual a zero,
-                                                                                insere na lista resultante*/
+                if((p1->coef - p2->coef) != 0)              /*se o coeficiente p1 menos o
+                                                            coeficiente de p2 não for igual a zero,
+                                                            insere na lista resultante*/
                 {
                     polinomioResultante = insereOrdenado(polinomioResultante, (p1->coef - p2->coef), p1->exp);
                     p1 = p1->proximo;
                     p2 = p2->proximo;
                 }
-                else                                                            /*se não aponta para o proximo*/
+                else                                       /*se não aponta para o proximo*/
                 {
                     p1 = p1->proximo;
                     p2 = p2->proximo;
                 }
             }
-            else                                                                /*se os coeficientes forem diferentes*/
+            else                                          /*se os coeficientes forem diferentes*/
             {
-                if(p1->exp > p2->exp)                                           /*se o expoente de p1 é maior do que o de p2,
-                                                                                insere p1 e aponta para o proximo*/
+                if(p1->exp > p2->exp)                     /*se o expoente de p1 é maior do que o de p2,
+                                                          insere p1 e aponta para o proximo*/
                 {
                     polinomioResultante = insereOrdenado(polinomioResultante, p1->coef, p1->exp);
                     p1 = p1->proximo;
                 }
-                else                                                            /*se nao insere p2 e aponta para o proximo*/
+                else                                      /*se nao insere p2 e aponta para o proximo*/
                 {
                     polinomioResultante = insereOrdenado(polinomioResultante, (p2->coef* (-1)), p2->exp);
                     p2 = p2->proximo;
                 }
             }
         }
-        else                                                                    /*Se uma das listas for nula*/
+        else                                              /*Se uma das listas for nula*/
         {
-            if(p1 != NULL)                                                      /*Se p1 não for o nulo insere na
-                                                                                lista resultante e aponta para o proximo*/
+            if(p1 != NULL)                                /*Se p1 não for o nulo insere na
+                                                          lista resultante e aponta para o proximo*/
             {
                 polinomioResultante = insereOrdenado(polinomioResultante, p1->coef, p1->exp);
                 p1 = p1->proximo;
             }
-            if(p2 != NULL)                                                      /*Se p2 não for o nulo insere na
-                                                                                lista resultante e aponta para o proximo*/
+            if(p2 != NULL)                                 /*Se p2 não for o nulo insere na
+                                                           lista resultante e aponta para o proximo*/
             {
                 polinomioResultante = insereOrdenado(polinomioResultante, -p1->coef, p1->exp);
                 p1 = p1->proximo;
@@ -158,6 +187,7 @@ No* subtrairPolinomios(No* polinomio1, No* polinomio2)
     return polinomioResultante;
 }
 
+/* A função abaixo multiplica dois polinômios*/
 No* multiplicaPolinomio(No *l1,No *l2)
 {
 
@@ -173,68 +203,24 @@ No* multiplicaPolinomio(No *l1,No *l2)
         while(p2!=NULL)
         {
 
-            if(p1->exp!=0 && p2->exp==0)
+            if(p1->exp!=0 && p2->exp==0) ///Se o expoente do segundo polinômio for zero
                 l3 = insereOrdenado(l3,p1->coef*p2->coef,p1->exp);
 
-            else if(p1->exp==0 && p2->exp!=0)
+            else if(p1->exp==0 && p2->exp!=0) ///Se o expoente do primeiro polinômio for zero
                 l3 = insereOrdenado(l3,p1->coef*p2->coef,p2->exp);
             else
-                l3 = insereOrdenado(l3,p1->coef*p2->coef,p1->exp+p2->exp);
+                l3 = insereOrdenado(l3,p1->coef*p2->coef,p1->exp+p2->exp); ///Se os dois expoentes forem diferentes de 0
             p2=p2->proximo;
         }
         p2=l2;
         p1=p1->proximo;
     }
-    
+
     l3=simplificaPolinomio(l3);
 
     /**Gravar no arquivo**/
 
     return l3;
-}
-
-No* simplificaPolinomio(No *l)
-{
-
-    No *atual,*post;
-
-    atual=l;
-    post=l->proximo;
-
-    while(post!=NULL)
-    {
-
-        if(atual->exp==post->exp)
-        {
-            post->coef+=atual->coef;
-            l=removeNo(l,atual);
-        }
-        atual=post;
-        post=post->proximo;
-    }
-    return l;
-}
-
-void resultadoPolinomio(No *l)
-{
-
-    float x,res=0;
-    No *p;
-
-    scanf("%f",&x);
-    p=l;
-
-    while(p!=NULL)
-    {
-
-        res+=p->coef*pow(x,p->exp);
-        p=p->proximo;
-    }
-
-    printf("Resultado: %f\n",res);
-
-    /*TODO
-    ESCREVER RESULTADO NO ARQUIVO!*/
 }
 
 ResultadoDivisao* dividirPolinomios(No* polinomio1, No* polinomio2)
@@ -293,33 +279,57 @@ ResultadoDivisao* dividirPolinomios(No* polinomio1, No* polinomio2)
     return resultado;                                               /*retorna o resuldado*/
 }
 
-No *somaPolinomio(No *l1,No *l2)
+///DERIVADA.
+
+/* Nesta função compara-se nó a nó para verificar se é possível a simplificação
+então soma-se os coeficientes e elimina-se um nó */
+No* simplificaPolinomio(No *l)
 {
 
-    No *l3,*p1,*p2;
+    No *atual,*post;
 
-    l3=criaLista();
-    p1=l1;
-    p2=l2;
+    atual=l;
+    post=l->proximo;
 
-    while(p1!=NULL || p2!=NULL)
+    while(post!=NULL)
     {
-        if(p1!=NULL)
+
+        if(atual->exp==post->exp)
         {
-            l3=insereOrdenado(l3,p1->coef,p1->exp);
-            p1=p1->proximo;
+            post->coef+=atual->coef;
+            l=removeNo(l,atual);
         }
-        if(p2!=NULL)
-        {
-            l3=insereOrdenado(l3,p2->coef,p2->exp);
-            p2=p2->proximo;
-        }
+        atual=post;
+        post=post->proximo;
     }
-    imprimeLista(l3);
-    l3=simplificaPolinomio(l3);
-    return l3;
+    return l;
 }
 
+/* A função lê um valor real e calcula o resultado do polinômio */
+void resultadoPolinomio(No *l)
+{
+
+    float x,res=0;
+    No *p;
+
+    scanf("%f",&x);
+    p=l;
+
+    while(p!=NULL)
+    {
+
+        res+=p->coef*pow(x,p->exp);
+        p=p->proximo;
+    }
+
+    printf("Resultado: %f\n",res);
+
+    /*TODO
+    ESCREVER RESULTADO NO ARQUIVO!*/
+}
+
+/* A composição calcula o valor inserido em um polinômio e
+pega esse resultado calculando no proximo polinomio  */
 float compostoPolinomio(No *l1,No *l2,float v){
 
     No *q=l1;
@@ -332,7 +342,9 @@ float compostoPolinomio(No *l1,No *l2,float v){
 
     return res;
 }
-
+/* Na enésima composição o valor de x é recebido,
+depois polinômio é lido e calcula-se o valor para cada polinômio inserido
+sempre usando o ultimo resultado calculado*/
 float n_compostoPolinomio(float v){
 
     char stringPolinomio[MAX];
@@ -373,11 +385,10 @@ void imprimeLOG(FILE *log){ /** Funcao que imprime o LOG **/
     if(log == NULL){ printf("Erro, nao foi possivel abrir o arquivo\n");}
 
     else{
-            while(fscanf(log,"%s", &s) != EOF){ /* Printa linha a linha */ 
+            while(fscanf(log,"%s", &s) != EOF){ /* Printa linha a linha */
                     printf("%s \n", s);
             }
         }
 
     fclose(log);
 }
-
